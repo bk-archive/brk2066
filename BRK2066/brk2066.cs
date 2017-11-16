e02c6be90416d717d3f3165248f3c48a3de297a0
Byron Tardif <byvinyal@microsoft.com>
2017-11-16 14:33:25 -0800

    demo prep

--- BRK2066/brk2066.cs
+++ BRK2066/brk2066.cs
@@ -11,20 +11,12 @@ namespace BRK2066
     public static class brk2066
     {
         [FunctionName("brk2066")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage>Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            // parse query parameter
+                      // parse query parameter
             string name = req.GetQueryNameValuePairs()
                 .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
                 .Value;
-
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-
-            // Set name to query string or body data
-            name = name ?? data?.name;
 
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
